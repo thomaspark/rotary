26690ead1f1714ce013bec4db66496dac4c61c95
Thomas Park <thomas@thomaspark.co>
2016-08-31 05:54:53 -0400

    fix rotation bug for zepto

--- scripts/rotary.js
+++ scripts/rotary.js
@@ -53,17 +53,24 @@ var convertThetaToCssDegs = function(theta, rotation) {
 
 var getRotationDegrees = function(obj) {
   var angle;
-  var matrix = obj.css("-webkit-transform") ||
-  obj.css("-moz-transform")    ||
-  obj.css("-ms-transform")     ||
-  obj.css("-o-transform")      ||
-  obj.css("transform");
-  if(matrix !== 'none') {
+  var el = obj[0];
+  var st = window.getComputedStyle(el, null);
+
+  var matrix = st.getPropertyValue("-webkit-transform") ||
+               st.getPropertyValue("-moz-transform") ||
+               st.getPropertyValue("-ms-transform") ||
+               st.getPropertyValue("-o-transform") ||
+               st.getPropertyValue("transform");
+
+  if (matrix !== 'none') {
       var values = matrix.split('(')[1].split(')')[0].split(',');
       var a = values[0];
       var b = values[1];
       angle = Math.round(Math.atan2(b, a) * (180/Math.PI));
-  } else {angle = 0; }
+  } else {
+    angle = 0;
+  }
+
   return (angle < 0) ? angle + 360 : angle;
 };
 
